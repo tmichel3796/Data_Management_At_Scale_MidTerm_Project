37f29d53795bda4058192de9c3e3ba5ede3d20cc
tmichel3796 <tmichel3796@gmail.com>
2026-01-11 18:02:54 -0600

    Data_Management_At_Scale_Midterm_Project
    Uploading midterm project for data management at scale.
    
    Systematically download join and union over a hundred tables of data.

---
+++ columns.py
@@ -0,0 +1,4 @@
+activity_columns = [ "TUACTDUR24", "TUACTIVITY_N", "TUCASEID", "TUTIER1CODE", "TUTIER2CODE", "TUTIER3CODE"]
+respondent_columns =["TUCASEID","TULINENO","TUYEAR","weight"]
+roster_columns = ["TEAGE", "TERRP", "TESEX", "TUCASEID", "TULINENO"]
+who_columns = ["TUACTIVITY_N", "TUCASEID", "TULINENO"]
